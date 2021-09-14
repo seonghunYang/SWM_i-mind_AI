7cf3c85ec9040cecb0a1755f8284f14d7f04cb1d
DrMaemi <leesh1510@ajou.ac.kr>
2021-09-14 15:39:18 +0900

    로그 파일 저장 위치 변경, 로그 형식 json으로 변환, json.dumps()로 홑따옴표를 쌍따옴표로 출력

--- demo/catch_logger.py
+++ demo/catch_logger.py
@@ -1,12 +1,13 @@
-import logging, os
+import logging, os, time
+import json
 
 class PersonalLogger():
     def __init__(self, path):
-        self.logger = logging.getLogger("PersonalLogger")
+        self.logger = logging.getLogger('PersonalLogger')
         
         self.logger.setLevel(logging.DEBUG)
 
-        formatter = logging.Formatter(fmt="%(asctime)s - %(message)s")
+        formatter = logging.Formatter(fmt='%(message)s')
 
         # stream(console) handler 객체 생성
         stream_handler = logging.StreamHandler()
@@ -16,21 +17,23 @@ class PersonalLogger():
 
         # file handler 객체 생성
         os.makedirs(path, exist_ok=True)
-        file_handler = logging.FileHandler(filename=path+'/personal.log')
+        file_handler = logging.FileHandler(filename=path+'/person.log')
         file_handler.setLevel(logging.INFO)
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
 
-        self.persons = []
-        self.personal_log = dict()
+        self.activities = []
+        self.personal_log = {}
 
     def add_person(self, input):
         name, action, x1, y1, x2, y2 = input
         person = {'name': name, 'action': action, 'pos': [x1, y1, x2, y2]}
-        self.persons.append(person)
+        self.activities.append(person)
 
     def log(self, frame_num):
-        self.personal_log['person'] = self.persons
-        self.personal_log['frame'] = frame_num
-        self.logger.info(self.personal_log)
-        self.persons = []
+        now = time.localtime()
+        self.personal_log['time'] = f'{now.tm_year:04d}-{now.tm_mon:02d}-{now.tm_mday:02d} {now.tm_hour:02d}:{now.tm_min:02d}:{now.tm_sec:02d}'
+        self.personal_log['activities'] = self.activities
+        self.personal_log['fn'] = frame_num
+        self.logger.info(json.dumps(self.personal_log))
+        self.activities = []
