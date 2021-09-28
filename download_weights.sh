80b43db3bedbe08d800ff30c154ccbfdc89051b4
DrMaemi <leesh1510@ajou.ac.kr>
2021-09-28 22:18:22 +0900

    모델 경로 생성 및 위치 자동화

--- download_weights.sh
+++ download_weights.sh
@@ -9,3 +9,15 @@ wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download
 
 # yolov3-spp.weight
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-D-bk-8XuUK4n3Vfv8YU0EdlZsH2y0I8' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-D-bk-8XuUK4n3Vfv8YU0EdlZsH2y0I8" -O yolov3-spp.weights && rm -rf /tmp/cookies.txt
+
+# make paths
+mkdir -p ./data/models/detector_models/
+mkdir -p ./detector/Yolov5_DeepSort_Pytorch/yolov5/weights/
+mkdir -p ./demo/model_data/models/
+mkdir -p ./data/models/aia_models/
+
+# place model weights
+mv yolov3-spp.weights ./data/models/detector_models
+mv yolov5s.pt ./detector/Yolov5_DeepSort_Pytorch/yolov5/weights
+mv model.pth ./demo/model_data/models
+mv resnet101_8x8f_denseserial.pth ./data/models/aia_models
