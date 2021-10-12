b1f41ba03cf606bba8878e0e9750389ab09a17cf
Ubuntu <ubuntu@ip-172-31-3-203.ap-northeast-2.compute.internal>
2021-10-12 08:37:53 +0000

    표정 인식 로깅 모듈 추가

--- demo/catch_logger.py
+++ demo/catch_logger.py
@@ -30,10 +30,35 @@ class PersonalLogger():
         person = {'name': name, 'action': action, 'pos': [x1, y1, x2, y2]}
         self.activities.append(person)
 
-    def log(self, frame_num):
+    def log(self, frame_num, timestamp):
         now = time.localtime()
         self.personal_log['time'] = f'{now.tm_year:04d}-{now.tm_mon:02d}-{now.tm_mday:02d} {now.tm_hour:02d}:{now.tm_min:02d}:{now.tm_sec:02d}'
         self.personal_log['activities'] = self.activities
         self.personal_log['fn'] = frame_num
+        self.personal_log['timestamp'] = timestamp
         self.logger.info(json.dumps(self.personal_log))
         self.activities = []
+
+class EmotionLogger():
+    def __init__(self, path):
+        self.logger = logging.getLogger('EmotionLogger')
+        
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(fmt='%(message)s')
+
+        # stream(console) handler 객체 생성
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.WARNING)
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(stream_handler)
+
+        # file handler 객체 생성
+        os.makedirs(path, exist_ok=True)
+        file_handler = logging.FileHandler(filename=path+'/emotion.log')
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+    def log(self, face_boxes_and_emotions_by_timestamp):
+        self.logger.info(json.dumps(face_boxes_and_emotions_by_timestamp))
